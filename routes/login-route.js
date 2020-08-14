1a2dc06095b10e5e9ca046de38012e61e5f5a9cb
Soraia Veríssimo <soraia@iMac.Home>
2020-08-14 21:33:35 +0000

    fixed bug in failed login

--- routes/login-route.js
+++ routes/login-route.js
@@ -7,8 +7,12 @@ router.post("/", async (req, res) => {
   let password = crypto.MD5(req.body.password).toString();
   let username = req.body.username;
 
+  try{
   let result = await userService.authenticate(username, password);
   res.send(result);
+  } catch(error){
+    res.send({});
+  }
 });
 
 module.exports = router;
